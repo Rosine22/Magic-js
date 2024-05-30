168c3ef04f8e9af9386ffd73f7e23dab853febc6
Rosine22 <r.uwineza@alustudent.com>
2024-05-30 12:36:18 +0200

    updates

--- Dog_years.js
+++ Dog_years.js
@@ -8,7 +8,7 @@ let laterYears = myAge - 2;
 laterYears *= 4;
 console.log(earlyYears);
 console.log(laterYears);
-//adding earlyYears and laterYears to get myAgeInDogYears
+//adding earlyYears and laterYears to get myAgeInDogYes
 let myAgeInDogYears = earlyYears + laterYears ;
 let myName = 'Rosine Uwineza'.toLowerCase();
 console.log(`My name is ${myName}. I am ${myAge} years old in
